abc097f352312011251e650ee2d349f33d84cc83
CrazyZhao <271381573@qq.com>
2015-11-24 18:12:42 +0800

    20151124

--- src/com/itheima/zhbj52/base/TabDetailPager.java
+++ src/com/itheima/zhbj52/base/TabDetailPager.java
@@ -1,35 +1,126 @@
 package com.itheima.zhbj52.base;
 
+import java.io.UTFDataFormatException;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.graphics.Color;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.itheima.zhbj52.R;
 import com.itheima.zhbj52.domain.NewsData.NewsMenuData.NewsTabData;
+import com.itheima.zhbj52.domain.TabData;
+import com.itheima.zhbj52.domain.TabData.TopNewsData;
+import com.itheima.zhbj52.global.GlobalConstants;
+import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 public class TabDetailPager extends BaseMenuDetailPager {
 
 	private NewsTabData mNewsTabData;
 	private TextView tvText;
+	private String mUrl;
+	private TabData mTabData;
+
+	@ViewInject(R.id.vp_news)
+	private ViewPager mViewPager;
+	
+	private ArrayList<TopNewsData> mTopNewsList;// 头条新闻数据集合
 
 	public TabDetailPager(Activity mActivity, NewsTabData newsTabData) {
 		super(mActivity);
 		mNewsTabData = newsTabData;
+		mUrl = GlobalConstants.SERVER_URL + mNewsTabData.url;
 	}
 
 	@Override
 	public View initView() {
-		tvText = new TextView(mActivity);
-		tvText.setTextColor(Color.RED);
-		tvText.setTextSize(25);
-		tvText.setGravity(Gravity.CENTER);
-		return tvText;
+		View view = View.inflate(mActivity, R.layout.tab_detail_pager, null);
+		ViewUtils.inject(this, view);
+		return view;
 	}
 
 	@Override
 	public void initData() {
-		tvText.setText(mNewsTabData.title);
+		getDataFromServer();
 	}
 
+	private void getDataFromServer() {
+		HttpUtils utils = new HttpUtils();
+		utils.send(HttpMethod.GET, mUrl, new RequestCallBack<String>() {
+
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+				String result = responseInfo.result;
+				System.out.println("页签详情页返回结果：" + result);
+				parseData(result);
+			}
+
+			@Override
+			public void onFailure(HttpException error, String msg) {
+				Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT).show();
+				error.printStackTrace();
+			}
+		});
+	}
+
+	protected void parseData(String result) {
+		Gson gson = new Gson();
+		mTabData = gson.fromJson(result, TabData.class);
+		
+		mTopNewsList = mTabData.data.topnews;
+		mViewPager.setAdapter(new TopNewsAdapter());
+	}
+
+	/**
+	 * 头条新闻适配器
+	 */
+	class TopNewsAdapter extends PagerAdapter {
+		
+		BitmapUtils bitmapUtils;
+		public TopNewsAdapter(){
+			bitmapUtils = new BitmapUtils(mActivity);
+			bitmapUtils.configDefaultLoadingImage(R.drawable.topnews_item_default);//设置默认加载的图片
+		}
+		
+		@Override
+		public int getCount() {
+			return mTabData.data.topnews.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return view == object;
+		}
+		
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			ImageView mImage = new ImageView(mActivity);;
+			mImage.setScaleType(ScaleType.FIT_XY); //基于控件大小填充图片
+			TopNewsData topNewsData = mTopNewsList.get(position);
+			bitmapUtils.display(mImage, topNewsData.topimage); //传入imageView对象和图片地址
+			container.addView(mImage);
+			return mImage;
+		}
+		
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			container.removeView((View)object);
+		}
+	}
 }
---
+++ src/com/itheima/zhbj52/domain/TabData.java
@@ -0,0 +1,69 @@
+package com.itheima.zhbj52.domain;
+
+import java.util.ArrayList;
+
+/**
+ * 页签详情页数据
+ * @author baoliang.zhao
+ *
+ */
+public class TabData {
+
+	public String retCode;
+	
+	public TabDetail data;
+	
+	public class TabDetail {
+		public String title;
+		public String more;
+		public ArrayList<TabNewsData> news;
+		public ArrayList<TopNewsData> topnews;
+		
+		@Override
+		public String toString() {
+			return "TabDetail [title=" + title + ", news=" + news
+					+ ", topnews=" + topnews + "]";
+		}
+	}
+	
+	/**
+	 * 新闻列表
+	 * @author baoliang.zhao
+	 *
+	 */
+	public class TabNewsData {
+		public String id;
+		public String listimage;
+		public String pubdate;
+		public String title;
+		public String type;
+		public String url;
+		@Override
+		public String toString() {
+			return "TabNewsData [title=" + title + "]";
+		}
+	}
+	/**
+	 * 头条新闻
+	 * @author baoliang.zhao
+	 *
+	 */
+	public class TopNewsData {
+		public String id;
+		public String topimage;
+		public String pubdate;
+		public String title;
+		public String type;
+		public String url;
+		@Override
+		public String toString() {
+			return "TopNewsData [title=" + title + "]";
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "TabData [data=" + data + "]";
+	}
+	
+}
